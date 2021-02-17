f0e258ab4580d007e425980fd4a2667442568f93
Christian Montes <christian.montes8991@gmail.com>
2021-02-17 02:34:13 -0800

    added root elem to DOM

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import _ from "lodash"
 
 class Welcome extends React.Component {
   render() {
     return <h1>Hello World from React boilerplate</h1>;
   }
 }
+
+const containerElem = document.createElement('div');
+containerElem.setAttribute('id', 'root');
+document.body.appendChild(containerElem);
 
 ReactDOM.render(<Welcome />, document.getElementById("root"));
