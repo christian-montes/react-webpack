d85da032368618af2bc98b41f2e2e1afdea8e9df
Christian Montes <christian.montes8991@gmail.com>
2021-02-16 23:02:39 -0800

    adding webpack module rules

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,34 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+
+            {
+                test: /\.scss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+
+            {
+                test: /\.(csv|tsv)$/i,
+                use: [csv-loader],
+            },
+
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            }
+        ],
+    },
 }
